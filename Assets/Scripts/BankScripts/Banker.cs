40f497c0818119009a086ea59164732ed31507c4
DreamingJohnny <johnnydreaming@gmail.com>
2021-02-03 09:29:53 +0100

    Sketched up the scripts that banker will need

---
+++ Assets/Scripts/BankScripts/Banker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banker : MonoBehaviour
+{
+    public float movement;
+    public float health;
+    public float damage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If not impacted by zombie, moves towards goal.
+        //BankerMoving()
+
+        //If impacted by zombie/as in close by zombie, stands still & attacks zombie.
+        //BankerAttacking()
+
+        //
+    }
+}
