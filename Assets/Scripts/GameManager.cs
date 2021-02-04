528764d85b9e55aef9261ac2ce02cd1e7f81c2d4
gwsmiddelkoop <gwsmiddelkoop@gmail.com>
2021-02-04 10:42:59 +0100

    Fixed Everything

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Properties")]
+    public int startingAmount;
+
+    void Start()
+    {
+        CurrencyManager.Instance.ModifyCurrency(startingAmount);
+    }
 
     public void LoseGame()
     {
