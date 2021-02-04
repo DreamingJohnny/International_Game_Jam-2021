58edbd4bf74423ade95af67cf0b92754e42896f0
DreamingJohnny <johnnydreaming@gmail.com>
2021-02-04 11:21:05 +0100

    Set up part of the UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,20 +5,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Canvas mainMenuCanvas;
+    public Canvas gamePlayCanvas;
+    public Canvas winCanvas;
+    public Canvas gameOverCanvas;
+
+    private void Start()
+    {
+        mainMenuCanvas.enabled = false;
+
+        gameOverCanvas.enabled = false;
+        
+        gamePlayCanvas.enabled = true;
+
+        winCanvas.enabled = false;
+    }
 
     public void LoseGame()
     {
+        gamePlayCanvas.enabled = false;
+
+        gameOverCanvas.enabled = true;
 
         Debug.Log("Your farm was destroyed!");
     }
 
-    public void RestartGame()
+    public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        mainMenuCanvas.enabled = false;
+
+        gamePlayCanvas.enabled = true;
+    }
+
+    public void GotoMainMenu()
+    {
+        gamePlayCanvas.enabled = false;
+
+        mainMenuCanvas.enabled = true;
     }
 
     public void WinGame()
     {
         Debug.Log("Bank Destroyed");
+
+        gamePlayCanvas.enabled = false;
+
+        winCanvas.enabled = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
